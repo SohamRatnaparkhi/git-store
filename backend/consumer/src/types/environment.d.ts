7060944fc5f0ad79d90e4334a09154f00328d73f
SohamRatnaparkhi <soham.ratnaparkhi@gmail.com>
2023-12-20 19:35:42 +0530

    chore: add env vars
    Signed-off-by: SohamRatnaparkhi <soham.ratnaparkhi@gmail.com>

---
+++ backend/consumer/src/types/environment.d.ts
@@ -0,0 +1,16 @@
+import Jwt from "jsonwebtoken";
+
+declare global {
+    namespace NodeJS {
+        interface ProcessEnv {
+            NODE_ENV: 'development' | 'production';
+            PORT?: string;
+            KAFKA_BROKER1?: string;
+            GH_APP_ID?: string;
+            GH_APP_PRIVATE_KEY: Jwt.Secret;
+            GH_APP_WEBHOOK_SECRET: string;
+        }
+    }
+}
+
+export {}
