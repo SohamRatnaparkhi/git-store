b2ebe507e59c65b647f0d2ddf33dd9249a0ccff2
SohamRatnaparkhi <soham.ratnaparkhi@gmail.com>
2023-12-25 20:32:38 +0530

    feat: enhance error handling

--- backend/consumer/src/helpers/consumer-handlers/prs.ts
+++ backend/consumer/src/helpers/consumer-handlers/prs.ts
@@ -1,21 +1,46 @@
 import { filteredPullRequest } from "src/types/github";
 import { cloneRepo } from "../github/clone";
 import fs from 'fs';
+import { helperResponse } from "src/types/server";
 
-export const handleGithubPrsClosedEvent = async (message: filteredPullRequest) => {
-    console.log("Handling closed pull request event");
-    console.log(message);
-    const {message: repoCloneStatus, path} = await cloneRepo(message.sender.login, message.repository.name, message.repository.private);
-    console.log(repoCloneStatus, path);
+export const handleGithubPrsClosedEvent = async (message: filteredPullRequest): Promise<helperResponse<any>> => {
+    try {
+        console.log("Handling closed pull request event");
+        console.log(message);
+        const { status: repoCloneStatus, data: path } = await cloneRepo(message.sender.login, message.repository.name, message.repository.private);
+        console.log(repoCloneStatus, path);
 
-    // check if zip successful
-    if (fs.existsSync(path)) {
-        console.log("Zip file exists");
+        if (repoCloneStatus === 'error' || !path) {
+            return {
+                status: 'error',
+                message: 'error while cloning repo',
+            };
+        }
+
+        // check if zip successful
+        if (fs.existsSync(path)) {
+            console.log("Zip file exists");
+        } else {
+            return {
+                status: 'error',
+                message: 'error while zipping repo',
+            }
+        }
+
+        return {
+            status: 'success',
+            message: 'successfully handled closed pull request event',
+            data: path
+        }
+    } catch (error) {
+        return {
+            status: 'error',
+            message: 'error while handling closed pull request event',
+            error
+        }
     }
-
+    //! TODO
     // backup zip file to s3/gcp/ipfs-filecoin/ipfs-lighthouse/ipfs-sphereon
     // add backup to db
     // send message to frontend to update
-
-    return;
 }
