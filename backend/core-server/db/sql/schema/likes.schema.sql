6a9f3a91ed0a1966d900024882517fc3c9f1b2b0
SohamRatnaparkhi <soham.ratnaparkhi@gmail.com>
2024-01-18 11:55:11 +0530

    fix: (MAJOR) update schmeas to include and link with product
    Signed-off-by: SohamRatnaparkhi <soham.ratnaparkhi@gmail.com>

--- backend/core-server/db/sql/schema/likes.schema.sql
+++ backend/core-server/db/sql/schema/likes.schema.sql
@@ -1,10 +1,10 @@
 CREATE TABLE
     Likes (
         like_id UUID PRIMARY KEY,
-        release_id UUID NOT NULL,
+        product_id UUID NOT NULL,
         user_id UUID NOT NULL,
         created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
         updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        FOREIGN KEY (release_id) REFERENCES Release(release_id) ON DELETE CASCADE,
+        FOREIGN KEY (product_id) REFERENCES Product(product_id) ON DELETE CASCADE,
         FOREIGN KEY (user_id) REFERENCES Users(user_id) ON DELETE CASCADE
     )
